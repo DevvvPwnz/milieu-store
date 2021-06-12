a8612edd69f1631df674a4133efc714e0649c1f0
DevvvPwnz <ddl1zator@gmail.com>
2021-06-12 20:20:44 +0300

    json sever update

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
-server.use(express.static(path.join(__dirname, 'build')));
-server.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'build')));
 
+
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 server.listen(PORT, () => {
   console.log('Server is running');
 });
